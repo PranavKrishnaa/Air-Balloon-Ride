2926c56ce2f70fc517b52283178a312171b04b07
PranavKrishnaa <81923726+PranavKrishnaa@users.noreply.github.com>
2021-08-15 09:56:23 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -13,10 +13,10 @@ function preload(){
 //Function to set initial environment
 function setup() {
   database = firebase.database();
-  console.log(database);
+  //console.log(database);
   createCanvas(1500,700);
 
-  balloon=createSprite(250,450,150,150);
+  balloon=createSprite(300,500,150,150);
   balloon.addAnimation("hotAirBalloon",balloonImage1);
   balloon.scale=0.5;
 
@@ -33,25 +33,27 @@ function draw() {
   background(bg);
 
   if(keyDown(LEFT_ARROW)){
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in left direction
     updateHeight(-10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+
   }
   else if(keyDown(RIGHT_ARROW)){
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in right direction
     updateHeight(10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+
   }
   else if(keyDown(UP_ARROW)){
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in up direction
     updateHeight(0,-10)
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
     balloon.scale = balloon.scale - 0.01;
   }
   else if(keyDown(DOWN_ARROW)){
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in down direction
     updateHeight(0,10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
     balloon.scale = balloon.scale + 0.01;
   }
 
